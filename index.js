35ca35e83221042c3f327cbcfa879107e1c99801
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-07-03 18:50:48 +0300

    Last Commit

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = function(){
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
